cb517ea77eddde0f09bf45aee89ccd35cf4b99f4
jantore <jantore@32k.org>
2009-06-06 16:44:21 +0200

    make randomize function compatible with MySQL, PostgreSQL and SQLite

--- lib/Myriad/Schema/ResultSet/Peer.pm
+++ lib/Myriad/Schema/ResultSet/Peer.pm
@@ -21,15 +21,37 @@ sub incomplete {
 }
 
 sub randomize {
-    return shift->search({}, { order_by => 'RAND()' });
+    my $self = shift;
+
+    ###
+    # We wish to randomize on the database side, so we need to determine
+    # what random function we want to use.
+    ##
+    my $type = $self->result_source->storage->sqlt_type;
+
+    my $random;
+    if($type eq 'MySQL') {
+        $random = 'RAND()';
+    } elsif($type eq 'PostgreSQL' || $type eq 'SQLite') {
+        $random = 'RANDOM()';
+    } else {
+        carp "Unrecognized database driver, no randomizing done";
+        return $self;
+    }
+
+    return $self->search({}, { order_by => $random });
 }
 
 sub descending {
-    return shift->search({}, { order_by => sprintf("%s DESC", shift) });
+    my ($self, $column) = @_;
+
+    return $self->search({}, { order_by => sprintf("%s DESC", $column) });
 }
 
 sub ascending {
-    return shift->search({}, { order_by => sprintf("%s ASC", shift) });
+    my ($self, $column) = @_;
+
+    return $self->search({}, { order_by => sprintf("%s ASC", $column) });
 }
 
 sub swarm_speed {
