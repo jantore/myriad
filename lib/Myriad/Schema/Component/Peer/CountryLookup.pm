1bb8ee8978c73f39fa7a1085a6b0ef5a276e64cb
jantore <jantore@32k.org>
2009-06-06 09:41:25 +0200

    don't depend on IP::Country::Fast, but allow use if available

--- lib/Myriad/Schema/Component/Peer/CountryLookup.pm
+++ lib/Myriad/Schema/Component/Peer/CountryLookup.pm
@@ -2,14 +2,30 @@ package Myriad::Schema::Component::Peer::CountryLookup;
 
 use base qw{ DBIx::Class };
 
-use IP::Country::Fast;
+use Carp;
 
 __PACKAGE__->mk_classdata('ipcountry');
-__PACKAGE__->ipcountry( IP::Country::Fast->new() );
+
+
+###
+# Attempt to load IP::Country::Fast, but don't fall apart if it's not
+# installed.
+##
+eval { require IP::Country::Fast; };
+unless($@) {
+    __PACKAGE__->ipcountry( IP::Country::Fast->new() );
+}
+
 
 sub country {
     my $self = shift;
-    return $self->ipcountry->inet_atocc( $self->address->addr );
+
+    if(defined $self->ipcountry) {
+        return $self->ipcountry->inet_atocc( $self->address->addr );
+    } else {
+        carp "No IP::Country::Fast instance loaded, module not installed?";
+        return undef;
+    }
 }
 
 1;
