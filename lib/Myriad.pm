44607f7cfbd2c5e2c69bce490c440fcc25932660
Jan Tore Morken <jantore@32k.org>
2014-07-14 18:22:15 +0200

    Add main module file.

---
+++ lib/Myriad.pm
@@ -0,0 +1,32 @@
+package Myriad;
+
+use strict;
+use version; our $VERSION = version->declare("v0.0.1");
+
+1;
+__END__
+
+=head1 NAME
+
+Myriad - a DBIx::Class-based BitTorrent tracker
+
+=head1 SYNOPSIS
+
+  my $myriad = Myriad::Schema->connect('dbi:mysql:database=tracker', '', '');
+  my $peers = $myriad->announce({});
+
+=head1 DESCRIPTION
+
+Myriad is a BitTorrent tracker backend that is suitable for use with
+Perl-based web application frameworks like L<Catalyst> or L<Mojolicious>.
+
+=head1 AUTHOR
+
+Jan Tore Morken <dist@jantore.net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2018-2014 by Jan Tore Morken.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
