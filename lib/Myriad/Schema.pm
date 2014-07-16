4f8be417e9544f98c4748ea6e9a0d86ce78bfed7
Jan Tore Morken <jantore@32k.org>
2014-07-16 21:06:36 +0200

    Add populate_test method in Myriad::Schema for populating test databases.

--- lib/Myriad/Schema.pm
+++ lib/Myriad/Schema.pm
@@ -6,4 +6,22 @@ use base qw{ DBIx::Class::Schema };
 
 __PACKAGE__->load_classes(qw{ Tracker Torrent Peer });
 
+
+sub populate_test {
+    my ($schema) = @_;
+
+    my $tracker = $schema->resultset('Tracker')->create({
+        host => 'tracker.example.net',
+        title => 'Test Tracker',
+        active => 1
+    });
+
+    my $torrent = $tracker->torrents->create({
+        info_hash => '00000000000000000000',
+        size      => 1000,
+        title     => 'Test Torrent',
+        active    => 1,
+    });
+}
+
 1;
